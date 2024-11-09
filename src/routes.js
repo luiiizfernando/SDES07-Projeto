610caf3eba2b50c46a8c9e9f3a6010e0e33f1c86
marcospaulorgomes <marcospaulorgomes.dev@gmail.com>
2024-11-08 21:31:40 -0300

    marcos.gomes: Adição do processo de ETL e conexão com o BD via Prisma

--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,140 @@
 import express from 'express';
 import { fetchGoogleBookReviewsByIsbns } from './services/googleBookService.js';
 import { fetchNytAllBestSellers, fetchAllIsbnsFromNytLists, fetchAllFromNytLists } from './services/newYorkTimesService.js';
+import { listDataFormater } from './utils/dataFormatter.js';
 import { dataBooks, searchDataBooks } from './services/dataProcessingService.js';
+import { googleReviewDataFormater, bookDataFormater } from './utils/googleBooksDataFormatter.js';
+import { livrosDaListaDataFormater } from './utils/newYorkTimesDataFormatter.js'
+import { bookExists, bookInsertion, listInsertion, booksOfListInsertion, booksOfListInsertionBS, reviewInsertion } from './repositories/databaseInsertions.js';
+import { PrismaClient } from '@prisma/client'
 
 const router = express.Router();
+const prisma = new PrismaClient();
 
-// Route to populate the database with books from Google Books API
+// Rota para testar a conexão e popular o banco com livros
 router.get('/create-book', async (req, res) => {
   try {
     let booksFormated = await dataBooks();
+    // booksFormated = arrayVerifier(booksFormated, 'isbn');
+    await Promise.all(booksFormated.map(async book => {
+      try {
+        if (book && typeof book === 'object' && book.hasOwnProperty('isbn')) {
+          if (book.isbn !== undefined && book.isbn !== "" && book.isbn.length === 13) {
+            await bookInsertion(book, prisma);  // Inserir livro
+          }
+        }
+      } catch (erro) {
+        console.log(erro);
+      }
+    }));
+
+    await prisma.$disconnect()
     res.json(booksFormated);
   } catch (err) {
     res.status(500).json({ error: 'Erro: ' + err });
   }
 });
 
-// Route to populate the database with NYT lists from NYT Books API
+// Rota para popular o banco de dados com as listas do NYT
 router.get("/create-lists", async (req, res) => {
   const nytLists = await fetchNytAllBestSellers();
   const lists = nytLists.results.filter(value => JSON.stringify(value) !== '{}');
-  res.json(lists);
+  const nytListsFormated = await Promise.all(lists.map(async list => {
+    return listDataFormater(list);
+  }));
+  const prisma = new PrismaClient();
+  nytListsFormated.forEach(formatedList => {
+    try {
+      listInsertion(formatedList, prisma);
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  await prisma.$disconnect();
+  res.json(nytListsFormated);
 });
 
-// Route to populate the database with the association of list from NYT Books API with books from Google Books API
 router.get("/create-books-list", async (req, res) => {
   const booksOfList = await fetchAllFromNytLists();
+  await Promise.all(booksOfList.map(async bookList => {
+    try {
+      if (!await bookExists(bookList.livro_isbn, prisma)) {
+        const book = await searchDataBooks(bookList.livro_isbn);
+        if (book) {
+          if (book.isbn !== undefined && book.isbn !== "" && book.isbn.length === 13) {
+            await bookInsertion(book, prisma);
+          }
+        }
+        if (await bookExists(bookList.livro_isbn, prisma)) {
+          booksOfList.forEach(booksList => {
+            try {
+              booksOfListInsertion(booksList, prisma);
+            } catch (error) {
+              console.log('Erro:', error);
+            }
+          });
+        }
+      } else {
+        booksOfList.forEach(booksList => {
+          try {
+            booksOfListInsertion(booksList, prisma);
+          } catch (error) {
+            console.log('Erro:', error);
+          }
+        });
+      }
+    } catch (error) {
+      console.log("Erro ao inserir livro da lista");
+    }
+  }));
+
+
+  await prisma.$disconnect();
   res.json(booksOfList);
 });
 
-router.get("/create-reviews", async (req, res) => {
-  // Consultar do reviews com base nos livros do db
+router.post("/create-reviews", async (req, res) => {
+  const prisma = new PrismaClient();
+
+  try {
+    // Obter livros com ISBNS do banco de dados
+    const booksFromDb = await prisma.livro.findMany({
+      select: {
+        isbn: true
+      }
+    });
+
+    // Extrair os ISBNS dos resultados
+    const reviewsIsbns = booksFromDb.map(book => book.isbn);
+
+    // Buscar reviews na API do Google Books usando os ISBNS
+    if (reviewsIsbns.length > 0) {
+      const reviews = await fetchGoogleBookReviewsByIsbns(reviewsIsbns);
+
+      // Processar e inserir reviews no banco de dados
+      for (const review of reviews) {
+        console.log(review);
+        try {
+          const formatedReview = googleReviewDataFormater(review);
+          await reviewInsertion(formatedReview, prisma);
+        } catch (error) {
+          console.error('Erro ao inserir review:', error);
+        }
+      }
+
+      res.json({ message: 'Reviews inseridas com sucesso!' });
+    } else {
+      res.json({ message: 'Nenhum ISBN encontrado no banco de dados.' });
+    }
+  } catch (error) {
+    console.error('Erro ao processar e inserir revisões:', error);
+    res.status(500).json({ error: 'Erro: ' + error });
+  } finally {
+    await prisma.$disconnect();
+  }
 });
 
+// Rota para tratar requisições não encontradas (404)
 router.get('*', (req, res) => {
   res.status(404).json({ error: 'Página não encontrada!' });
 });
---
+++ src/utils/dataFormatter.js
@@ -0,0 +1,75 @@
+export function listDataFormater(list) {
+    try {
+      let formatedList = {
+        nome: list.list_name,
+        data_publicacao: dateFormater(list.newest_published_date),
+        frequencia: list.updated,
+        livros_da_lista: list.books
+      };
+      return formatedList;
+    } catch (error) {
+      console.log("Erro de formatação da lista: " + error);
+    }
+  }
+  
+  export function dateFormater(date) {
+    const [year, month, day] = date.split("-"); // "2024-06-06"
+    return new Date(year, parseInt(month) - 1, day).toISOString();
+  }
+  
+  export function arrayFormater(array) {
+    let booksList = [];
+    array.forEach(element => {
+      if (element) {
+        element.forEach(element2 => {
+          if (element2) {
+            if (element2.items) {
+              element2.items.forEach(book => {
+                if (book) booksList.push(book);
+              });
+            }
+          }
+        });
+      }
+    });
+    return booksList;
+  }
+  
+  
+  export function arrayFormater2(array) {
+    let booksList = [];
+    array.forEach(element => {
+      if (element) {
+        if (element.items) {
+          element.items.forEach(book => {
+            if (book) booksList.push(book);
+          });
+        }
+      }
+    });
+    return booksList;
+  }
+  
+  export function arrayVerifier(array, attribute) {
+    if (!Array.isArray(array)) {
+      throw new Error("Input must be an array");
+    }
+    if (!attribute) {
+      throw new Error("Attribute must be defined");
+    }
+  
+    let reduced = [];
+    array.forEach((item) => {
+      if (item && item.hasOwnProperty(attribute)) {
+        let duplicated = reduced.findIndex(redItem => redItem[attribute] === item[attribute]) > -1;
+        if (!duplicated) {
+          reduced.push(item);
+        }
+      }
+    });
+    return reduced;
+  }
+  
+  export function sumArrays(array1, array2) {
+    return [...array1, ...array2];
+  }
